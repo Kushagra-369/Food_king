307374c532337514923c08569ec4f09253fc5361
Kushagra <kushagra100chhabra@gmail.com>
2025-05-20 18:15:59 +0530

    JU

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,17 +5,17 @@ import Onichepizza from './components/Onichepizza'
 import Onifinal from './components/Onifinal'
 import Onipizza from './components/Onipizza'
 import Onipizza2 from './components/Onipizza2'
-import Onisweetchepizza from './components/Onisweetchepizza'
-import Onisweetfinalpizza from './components/Onisweetfinal'
+// import Onisweetchepizza from './components/Onisweetchepizza'
+// import Onisweetfinalpizza from './components/Onisweetfinal'
 import Onisweetpizza from './components/Onisweetpizza'
 import Base from './components/Base'
 import Base2 from './components/Base2'
 import Base3 from './components/Base3'
 import Base4 from './components/Base4'
-import Chepizza from './components/Chepizza'
-import Sweetchepizza from './components/Sweetchepizza'
-import Sweetpizza from './components/Sweetpizza'
-import Sweetpizzafinal from './components/Sweetpizzafinal'
+// import Chepizza from './components/Chepizza'
+// import Sweetchepizza from './components/Sweetchepizza'
+// import Sweetpizza from './components/Sweetpizza'
+// import Sweetpizzafinal from './components/Sweetpizzafinal'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
@@ -34,14 +34,15 @@ export default function App() {
         <Route path='/onipizza2' element={<Onipizza2/>}/>
         <Route path='/onisweetpizza' element={<Onisweetpizza/>}/>
         <Route path='/base3' element={<Base3/>}/>
-        <Route path='/onifinal' element={<Onifinal/>}/>
         <Route path='/onichepizza' element={<Onichepizza/>}/>
+        <Route path='/onifinal' element={<Onifinal/>}/>
+        {/* 
         <Route path='/chepizza' element={<Chepizza/>}/>
         <Route path='/sweetpizza' element={<Sweetpizza/>}/>
         <Route path='/sweetpizzafinal' element={<Sweetpizzafinal/>}/>
         <Route path='/sweetchepizza' element={<Sweetchepizza/>}/>
         <Route path='/onisweetchepizza' element={<Onisweetchepizza/>}/>
-        <Route path='/onisweetfinal' element={<Onisweetfinalpizza/>}/>
+        <Route path='/onisweetfinal' element={<Onisweetfinalpizza/>}/> */}
 
         </Routes>
       </BrowserRouter>
