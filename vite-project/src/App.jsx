4923f0d264f94d705de61028c49bdce4d9d27e99
Kushagra <kushagra100chhabra@gmail.com>
2025-05-20 18:11:09 +0530

    KJ

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,8 +10,8 @@ import Onipizza2 from './components/Onipizza2'
 import Onisweetpizza from './components/Onisweetpizza'
 import Base from './components/Base'
 import Base2 from './components/Base2'
-// import Base3 from './components/Base3'
-// import Base4 from './components/Base4'
+import Base3 from './components/Base3'
+import Base4 from './components/Base4'
 // import Chepizza from './components/Chepizza'
 // import Sweetchepizza from './components/Sweetchepizza'
 // import Sweetpizza from './components/Sweetpizza'
@@ -29,14 +29,14 @@ export default function App() {
 
         <Route path='/' element={<Base/>}/>
         <Route path='/base2' element={<Base2/>}/>
-        {/* <Route path='/base4' element={<Base4/>}/> */}
+        <Route path='/base4' element={<Base4/>}/>
         <Route path='/onipizza' element={<Onipizza/>}/>
         <Route path='/onipizza2' element={<Onipizza2/>}/>
         <Route path='/onisweetpizza' element={<Onisweetpizza/>}/>
+        <Route path='/base3' element={<Base3/>}/>
         {/* <Route path='/onifinal' element={<Onifinal/>}/>
         <Route path='/onichepizza' element={<Onichepizza/>}/>
         <Route path='/chepizza' element={<Chepizza/>}/>
-        <Route path='/base3' element={<Base3/>}/>
         <Route path='/sweetpizza' element={<Sweetpizza/>}/>
         <Route path='/sweetpizzafinal' element={<Sweetpizzafinal/>}/>
         <Route path='/sweetchepizza' element={<Sweetchepizza/>}/>
